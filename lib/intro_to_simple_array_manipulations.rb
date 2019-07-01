fbc48abbb28a89b3a4a9bfdd239a6d23e36565c0
Anthony Carbajal <acarbajal721@live.com>
2019-07-01 22:54:20 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+
+def using_push(arr, str)
+  array.push()
+end
+
+def using_unshift(arr, str)
+  array.unshift(str)
+end
+
+def using_pop(arr)
+  return arr.pop()
+end
+
+def pop_with_args(arr, amt)
+  return arr.pop(amt)
+end
+
+def using_shift(arr)
+  return arr.shift()
+end
+
+def shift_with_args(arr, amt)
+  return arr.shift(amt)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, element)
+  arr1.insert(4, element)
+end
+
+def using_uniq(arr)
+  arr.uniq()
+end
+
+def using_flatten(arr)
+  return arr.flatten()
+end
+
+def using_delete(arr, str)
+    arr.delete(str)
+end
+
+def using_delete_at(arr, index)
+  arr.delete_at(index)
+end
+
+This method takes in two arguments, an `Array` and an `Integer` and deletes the
+element at the index of the array that is equal to that integer.
